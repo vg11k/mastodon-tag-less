3f8c465166c31954492b16a6177f4221d381ba18
vg11k <julien.vg11k@gmail.com>
2024-07-03 14:39:07 +0200

    lil update w async

--- src/content/mastodon-tag-less-content.js
+++ src/content/mastodon-tag-less-content.js
@@ -13,44 +13,75 @@
 
 	let limit = undefined;
 	let tagless_number_promise = browser.storage.local.get("mastodon_tagless_number_key");
-	tagless_number_promise.then(function(resultContainer) {
-		if(resultContainer !== undefined && Object.keys(resultContainer).length > 0) {
-			limit = resultContainer["mastodon_tagless_number_key"];
-		}
-	});
-
-	let foundDrawers = document.getElementsByClassName("item-list");
-	let validDrawer = [];
-	for(let drawer of foundDrawers) {
-		if(drawer.attributes.role.value === "feed") {
-			validDrawer.push(drawer);	
-			drawer.addEventListener("DOMNodeInserted", function (e) {
-				console.log("limit = " + limit);
-				if(limit !== undefined) {
-					for(let child of drawer.children) {
-						if(child.getAttribute("tagless") === null && child.children.length > 0) {
-							let subsubchild = child.children[0].children[0];
-							if(subsubchild !== undefined && subsubchild.classList.contains("status__wrapper")) {
-								let innerText = subsubchild.innerText;
-								let nbHashtags = innerText.split("#").length;
-								console.log(nbHashtags + " > " + limit + " ?");
-								if(nbHashtags > limit) {
-									child.style.display = "none";
-									child.setAttribute("tagless",true);
-								} 
-								else {
-									child.setAttribute("tagless",false);
-								}
-							}
+	
+	let asyncRetrieveLimit = function() {
+		tagless_number_promise.then(function(resultContainer) {
+			if(resultContainer !== undefined && Object.keys(resultContainer).length > 0) {
+				limit = resultContainer["mastodon_tagless_number_key"];
+				
+			}
+			
+			if(limit == undefined) {
+				console.log("Limite undefined, async");
+				setTimeout(asyncRetrieveLimit, 1000);
+			}
+			else {
+				console.log("Limite to apply : " + limit);
+				identifyDrawers();
+			}
+		});
+	};
+	
+	let clearDrawer = function(drawer) {
+		if(limit !== undefined) {
+			for(let child of drawer.children) {
+				if(child.getAttribute("tagless") === null && child.children.length > 0) {
+					let subsubchild = child.children[0].children[0];
+					if(subsubchild !== undefined && subsubchild.classList.contains("status__wrapper")) {
+						let innerText = subsubchild.innerText;
+						let nbHashtags = innerText.split("#").length;
+						if(nbHashtags > limit) {
+							child.style.display = "none";
+							child.setAttribute("tagless",true);
+						} 
+						else {
+							child.setAttribute("tagless",false);
 						}
-						
 					}
 				}
-
-			}, false);
+				
+			}
 		}
-	}
-
-
+	};
+	
+	
+	let identifyDrawers = function() {
+		console.log("identify drawers, limit : " + limit);
+		let foundDrawers = document.getElementsByClassName("item-list");
+		if(foundDrawers.length == 0) {
+			console.log("doc not ready");
+			setTimeout(identifyDrawers, 1000);
+		}
+		else {
+			doHashtagLessJob(foundDrawers);
+		}
+	};
+	
+	
+	let doHashtagLessJob = function(foundDrawers) {
+		console.log("dojob limit : " + limit + " drawers count : " + foundDrawers.length);
+		let validDrawer = [];
+		for(let drawer of foundDrawers) {
+			if(drawer.attributes.role.value === "feed") {
+				validDrawer.push(drawer);
+				clearDrawer(drawer);
+				drawer.addEventListener("DOMNodeInserted", function (e) {
+					clearDrawer(drawer);
+				}, false);
+			}
+		}
+	};
+	
+	setTimeout(asyncRetrieveLimit, 0);
 
 })();
